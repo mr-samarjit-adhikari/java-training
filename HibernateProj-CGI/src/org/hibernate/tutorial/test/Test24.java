f3498067da3eef02dfa8eb45e0633e6bb4ad2ddd
samarjit adhikari <adhikari@ADHIKARI4.asiapacific.hpqcorp.net>
2017-01-16 16:04:03 +0530

    add example of pessimistic locking

--- HibernateProj-CGI/src/org/hibernate/tutorial/test/Test24.java
+++ HibernateProj-CGI/src/org/hibernate/tutorial/test/Test24.java
@@ -2,25 +2,24 @@ package org.hibernate.tutorial.test;
 
 import javax.swing.JOptionPane;
 
+import org.hibernate.LockMode;
+import org.hibernate.LockOptions;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tutorial.model.PersonWithVersion;
 
-public class Test23 {
+public class Test24 {
 
 	/**
-	 * Learning Points:
-	 * 
-	 * Showing StaleObjectStateException 
-	 * when 2 concurrent users trying to update same entity and optimistic locking is used. 
-	 * another instance is Test23-2.java
+	 * learn pessimistic locking together with Test23-2.java
+	 * Run Test24.java first which will do the locking and then run Test23_2.java 
+	 * Test23_2.java will face StaleStateObject exception as it already read the object before committing 
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		
@@ -32,11 +31,8 @@ public class Test23 {
 			PersonWithVersion person = new PersonWithVersion();
             person.setName("Person 1");
             session.save(person);
-
-            System.out.println("Before commit: Name = " + person.getName() + ", Version = " + person.getVersion());  // version = 0
             tx.commit();
-            session.refresh(person);
-            System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());     // version = 0
+            tx = null;
 
             // 2nd commit
             tx = session.beginTransaction();
@@ -45,37 +41,20 @@ public class Test23 {
             tx.commit();
             session.refresh(person);
             System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());     // version = 1
-
-            // 3rd commit
-            tx = session.beginTransaction();
-            person.setName("Crazy Horse");
-            System.out.println("Before commit: Name = " + person.getName() + ", Version = " + person.getVersion());  // version = 1
-            tx.commit();
-            session.refresh(person);
-            System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());    // version = 2
-
-            // 4th commit
-            tx = session.beginTransaction();
-            person.setName("Beautie");
-            System.out.println("Before commit: Name = " + person.getName() + ", Version = " + person.getVersion());  // version = 2
-            tx.commit();
-            session.refresh(person);
-            System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());  // version = 3
             tx = null;
             
-            // Here we are going to display the dialog box asking
-            // a user to enter data.  This is to allow another user
-            // to change the database table.
+            //Now load the object with pessimistic locking 
             tx = session.beginTransaction();
+            person = (PersonWithVersion)session.load(PersonWithVersion.class, 1,new LockOptions(LockMode.PESSIMISTIC_FORCE_INCREMENT));
             String newName = "";
             newName = JOptionPane.showInputDialog("Please enter your name - you will experience an exception after another user changes the row");
             person.setName(newName);
+            
             System.out.println("Before commit: Name = " + person.getName() + ", Version = " + person.getVersion());
             tx.commit();
             session.refresh(person);
-            System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());
-
-            tx = null;
+            System.out.println("After commit: Name = " + person.getName() + ", Version = " + person.getVersion());       
+            tx = null;            
             
 		}
 		catch(Exception e){
@@ -88,6 +67,6 @@ public class Test23 {
 			session.close();
 		}
 		
-	}	
+	}
 
 }
