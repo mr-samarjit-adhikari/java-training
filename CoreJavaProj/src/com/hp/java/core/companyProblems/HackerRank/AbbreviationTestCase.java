e77faf308af531d03775bc1a0203f6cb07362059
samarjit adhikari <samarjit.adhikari@hpe.com>
2018-11-09 14:47:05 +0530

    another implementation of abbeviation

--- CoreJavaProj/src/com/hp/java/core/companyProblems/HackerRank/AbbreviationTestCase.java
+++ CoreJavaProj/src/com/hp/java/core/companyProblems/HackerRank/AbbreviationTestCase.java
@@ -3,6 +3,13 @@ package com.hp.java.core.companyProblems.HackerRank;
 import java.util.Scanner;
 
 public class AbbreviationTestCase {
+    public enum Match{
+        CHAR_MATCH,
+        CHAR_SKIP,
+        CHAR_MISMATCH,
+        UNKNOWN,
+    }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int testCaseCount = scanner.nextInt();
@@ -22,47 +29,32 @@ public class AbbreviationTestCase {
         int aStringLen = str1.length();
         char[] bStrArray = str2.toCharArray();
         char[] aStrArray = str1.toCharArray();
-        int aindex = 0;
+        Match [][] dp = new Match[bStringLen][aStringLen];
+        int aIndex = 0;
+        int bIndex = 0;
 
         if(aStringLen < bStringLen) return "NO";
 
-        for(int bindex=0;bindex<bStringLen;bindex++){
+        initializeDP(dp,bStringLen,aStringLen);
 
-            if(0==(aindex - aStringLen)) return "NO";
-
-            while(aindex<aStringLen){
-                if(bStrArray[bindex] == aStrArray[aindex])
-                {
-                    aindex++; break;
-                }
-                else if(aStrArray[aindex]>='a' && aStrArray[aindex]<='z'){
-                    if(bStrArray[bindex] == (aStrArray[aindex]-32)){
-                        if(bStrArray[bindex] == (aStrArray[aindex+1]-32)) {
-                            aindex++;
-                            continue;
-                        }
-                        else{
-                            aindex++; break;
-                        }
-                    }
-                    else if(aindex == aStringLen-1){
-                        return "NO";
-                    }
-                    else {
-                        aindex++;
-                        continue;
-                    }
-                }
-                else{
-                    return "NO";
-                }
-            }
+        dp[0][0] = compareChars(bStrArray[0],aStrArray[0]);
+        if(dp[0][0] == Match.CHAR_SKIP){
+            aIndex++;
+        }else if(dp[0][0] == Match.CHAR_MATCH){
+            aIndex++;bIndex++;
+        }else{
+            return "NO";
         }
 
-        if(aindex<aStringLen){
-            while (aindex< aStringLen){
-                if(aStrArray[aindex]>='a' && aStrArray[aindex]<='z'){
-                    aindex++; continue;
+        int aindex = aIndex;
+        for(int bindex=bIndex;bindex<bStringLen;bindex++){
+            while(aindex<aStringLen){
+                Match matchStatus = checkAndSet(bStrArray,bindex,aStrArray,aindex,dp);
+
+                if(matchStatus == Match.CHAR_SKIP){
+                    aindex++;continue;
+                }else if(matchStatus == Match.CHAR_MATCH){
+                    aindex++;break;
                 }
                 else{
                     return "NO";
@@ -71,5 +63,54 @@ public class AbbreviationTestCase {
         }
 
         return "YES";
+    }
+
+    private void initializeDP(Match[][] dp,int bStrlen,int aStrLen) {
+        for(int bindex=0;bindex<bStrlen;bindex++){
+            for(int aindex=0;aindex<aStrLen;aindex++){
+                dp[bindex][aindex] = Match.UNKNOWN;
+            }
+        }
+    }
+
+    private Match checkAndSet(char[] bStrArray, int bindex, char[] aStrArray, int aindex,Match dp[][]) {
+        Match lastMatch = Match.UNKNOWN;
+        Match currMatch = Match.UNKNOWN;
+
+        if((bindex-1) >= 0 && (aindex-1)>=0){
+            if(dp[bindex-1][aindex-1] != Match.UNKNOWN){
+                lastMatch =   dp[bindex-1][aindex-1];
+            }
+        }
+
+        if(bindex>=0 && (aindex-1) >= 0){
+            if(dp[bindex][aindex-1] != Match.UNKNOWN){
+                lastMatch = dp[bindex][aindex-1];
+            }
+        }
+
+        if(lastMatch != Match.UNKNOWN){
+            if((lastMatch == Match.CHAR_SKIP) || (lastMatch == Match.CHAR_MATCH)) {
+                dp[bindex][aindex]= currMatch = compareChars(bStrArray[bindex],aStrArray[aindex]);
+            }
+            else{
+                return Match.CHAR_MISMATCH;
+            }
+        }
+
+        return currMatch;
+    }
+
+
+    private Match compareChars(char bStrChar, char aStrChar) {
+
+        if ((aStrChar == bStrChar)||(bStrChar == aStrChar -32)) {
+            return Match.CHAR_MATCH;
+        }
+        else if(aStrChar >= 'a' && aStrChar<='z'){
+            return Match.CHAR_SKIP;
+        }
+
+        return Match.CHAR_MISMATCH;
     }
 }
