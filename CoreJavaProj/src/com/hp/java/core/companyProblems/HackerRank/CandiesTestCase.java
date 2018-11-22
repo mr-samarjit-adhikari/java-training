eafd90177ab3cca291fd40f4902b7f4a3f39b053
samarjit adhikari <samarjit.adhikari@hpe.com>
2018-11-23 00:06:34 +0530

    Fixed Candies TestCase

--- CoreJavaProj/src/com/hp/java/core/companyProblems/HackerRank/CandiesTestCase.java
+++ CoreJavaProj/src/com/hp/java/core/companyProblems/HackerRank/CandiesTestCase.java
@@ -18,21 +18,41 @@ public class CandiesTestCase {
     }
 
     private long solve(int[] arr) {
-        int candies = 0;
-        int[] dp = new int[arr.length]; //candies array
+        int[] candies = new int[arr.length];//candies array
+        int[] dp = new int[arr.length];
 
-        dp[0] = 1;candies++;
+        dp[0] = 1;
+        candies[0] = 1;
 
         for(int i=1;i<arr.length;i++){
-            if(arr[i] > arr[i-1]){
-                dp[i] = dp[i-1] +1;
-            }
-            else{
-                dp[i] = 1;
-            }
-            candies += dp[i];
+            dp[i] = dp[i-1] + compAndDist(arr,i,candies);
+            //System.out.println("i-"+i+"dp[i] - "+dp[i]);
         }
 
-        return candies;
+        return dp[arr.length-1];
+    }
+
+    private int compAndDist(int[] arr, int i, int[] candies) {
+        int allocatedCandies = 0;
+        if(0 == candies[i]) {
+            candies[i] = 1; //minimum allocation
+            allocatedCandies++;
+        }
+
+        if(arr[i] > arr[i-1]){
+            if(candies[i] <= candies[i-1]) {
+                candies[i] = candies[i - 1] + 1;
+                allocatedCandies += (candies[i]-1);
+            }
+        }
+        else if(arr[i-1] > arr[i]){
+            if(candies[i-1] <= candies[i]){
+                candies[i-1] = candies[i-1] +1;
+                allocatedCandies++;
+            }
+
+        }
+
+        return allocatedCandies;
     }
 }
